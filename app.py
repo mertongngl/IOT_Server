0fb13f752bae8328b65d46d3c7787866ef9927b7
mert <mert.ongengil@ni.tc>
2019-03-04 15:14:10 +0300

    mongodb tested

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask
+from mongodb_helper import MongodbHelper
 
 app = Flask(__name__)
 
@@ -9,7 +10,15 @@ def index():
 
 @app.route('/test')
 def test():
-    return "Test test test"
+    mongoHelper = MongodbHelper('data','mongodb://db:27017/','iot')
+    inserted_id = mongoHelper.add({"name":"mert","surname":"ongengil"})
+    return str(inserted_id)
+
+@app.route('/get')
+def getAllFuckinItems():
+    mongoHelper = MongodbHelper('data','mongodb://db:27017/','iot')
+    all_results = mongoHelper.get_all({})
+    return str(all_results)
 
 if __name__ == '__main__':
     app.run()
---
+++ mongodb_helper.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+
+
+class MongodbHelper:
+
+    def __init__(self, collection_name: str, db_dsn: str, db_name: str):
+        self.collection_name = collection_name
+        self.db_name = db_name
+        self.db_dsn=db_dsn
+        self.client = MongoClient(self.db_dsn)
+        self.db = self.client[self.db_name]
+
+    def __get_collection(self):
+        return self.db[self.collection_name]
+
+    def fetch_by(self, predicate):
+        result = self.__get_collection().find_one(predicate)
+        return result
+
+    def list(self, predicate):
+        result = self.__get_collection().find(predicate)
+        return result
+
+    def get_all(self, predicate: object = {}, fields: object = {}) -> object:
+        result = self.__get_collection().find(predicate, fields)
+        return result
+
+    def add(self, data):
+        result = self.__get_collection().insert_one(data)
+        return result.inserted_id
+
+    def edit(self, predicate, data):
+        result = self.__get_collection().update_one(predicate, {"$set": data})
+        return result.modified_count
+
+    def remove(self, predicate):
+        result = self.__get_collection().delete_one(predicate)
+        return result.deleted_count
