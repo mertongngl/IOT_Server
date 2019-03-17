82d9bf16bf810f66981bf04e2300e2eb912f3af5
mert <mert.ongengil@ni.tc>
2019-03-17 21:12:20 +0300

    payloads & sensorById created

--- api.py
+++ api.py
@@ -14,15 +14,6 @@ USER_DATA = {
     "admin": "SuperSecretPwd"
 }
 
-SENSORS = {
-    "wifi_id": fields.String(default="0"),
-    "type": fields.String(default=""),
-    "name": fields.String(default=""),
-    "description": fields.String(default=""),
-    "api_key": fields.String(default=""),
-    "last_connection_datetime": fields.DateTime()
-}
-
 @auth.verify_password
 def verify(username:str, password:str):
     if not (username and password):
@@ -30,20 +21,55 @@ def verify(username:str, password:str):
     return USER_DATA.get(username) == password
 
 class Sensors(Resource):
+    def __init__(self):
+        self.collection_name = "sensors"
+        self.db_dsn = "mongodb://db:27017/"
+        self.db_name = "iot"
     @auth.login_required
-    #@marshal_with(SENSORS, envelope='resource')
     def get(self):
-        mongoHelper = MongodbHelper('sensors', 'mongodb://db:27017/', 'iot')
+        mongoHelper = MongodbHelper(self.collection_name, self.db_dsn, self.db_name)
         response = list(mongoHelper.list({}))
         return api_response(200, response)
 
     @auth.login_required
     def post(self):
-        mongoHelper = MongodbHelper('sensors', 'mongodb://db:27017/', 'iot')
+        mongoHelper = MongodbHelper(self.collection_name, self.db_dsn, self.db_name)
         json_data = request.get_json(force=True)
         json_data['last_connection_datetime'] = datetime.datetime.now()
         result = mongoHelper.add(json_data)
-        return {"object_id":str(result)}
+        return api_response(201, {"object_id":result})
+
+class SensorById(Resource):
+    def __init__(self):
+        self.collection_name = "sensors"
+        self.db_dsn = "mongodb://db:27017/"
+        self.db_name = "iot"
+
+    @auth.login_required
+    def get(self, object_id):
+        mongoHelper = MongodbHelper(self.collection_name, self.db_dsn, self.db_name)
+        response = list(mongoHelper.fetch_by({'_id':object_id}))
+        return api_response(200, response)
+
+class Payloads(Resource):
+    def __init__(self):
+        self.collection_name = "payloads"
+        self.db_dsn = "mongodb://db:27017/"
+        self.db_name = "iot"
+    @auth.login_required
+    def post(self):
+        mongoHelper = MongodbHelper(self.collection_name, self.db_dsn, self.db_name)
+        json_data = request.get_json(force=True)
+        json_data['last_connection_datetime'] = datetime.datetime.now()
+        result = mongoHelper.add(json_data)
+        return api_response(201, {"object_id":result})
+
+    @auth.login_required
+    def get(self):
+        # hepsini çekmeyeceksin id ye göre çekeceksin çözmeye çalış
+        mongoHelper = MongodbHelper(self.collection_name, self.db_dsn, self.db_name)
+        response = list(mongoHelper.list({}))
+        return api_response(200, response)
 
 class HelloWorld(Resource):
     @auth.login_required
@@ -53,6 +79,8 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Sensors, '/sensors')
+api.add_resource(SensorById, '/sensors/<string:object_id>')
+api.add_resource(Payloads, '/payloads')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
